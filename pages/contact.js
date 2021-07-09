f9623278f98d6c0091ed542655555d8d05c7e43a
NishanthPrajith <nishanth.prajith@gmail.com>
2021-07-09 06:27:37 -0400

    fixed bug

--- pages/contact.js
+++ pages/contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
         <div className = {styles.container}>
             <div className = {styles.info}>
                 <div className = {styles.text}>
-                    <p className = {styles.title}>Let's get in Touch</p>
+                    <p className = {styles.title}>Let&apos;s get in Touch</p>
                     <div>
                         <p className = {styles.one}>I am currently available for work</p>
                         <p className = {styles.two}>nishanth.prajith@gmail.com</p>
