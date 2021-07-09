665093ef89ff8ae563d8364454750a6a3b1aba79
NishanthPrajith <nishanth.prajith@gmail.com>
2021-07-09 11:24:57 -0400

    added page active effect

--- comps/NavBar.js
+++ comps/NavBar.js
@@ -1,7 +1,11 @@
 import styles from '../styles/Nav.module.css'
 import Link from 'next/link'
+import { useRouter } from "next/router";
 
 const NavBar = () => {
+
+    const router = useRouter();
+
     return (
         <nav>
             <div className = {styles.FirstBox} data-aos = "fade-down">
@@ -14,22 +18,38 @@ const NavBar = () => {
             <div className = {styles.SecondBox}>
                 <div>
                     <ul>
-                        <li data-aos = "fade-down" data-aos-delay = "100">
+                        <li data-aos = "fade-down" data-aos-delay = "100" className={
+                                router.pathname === "/"
+                                ? styles.strike
+                                : null
+                            }>
                             <Link href = '/'>
                                 Home
                             </Link>
                         </li>
-                        <li data-aos = "fade-down" data-aos-delay = "200">
+                        <li data-aos = "fade-down" data-aos-delay = "200" className={
+                                router.pathname === "/about"
+                                ? styles.strike
+                                : null
+                            }>
                             <Link href = '/about'>
                                 About
                             </Link>
                         </li>
-                        <li data-aos = "fade-down" data-aos-delay = "300">
+                        <li data-aos = "fade-down" data-aos-delay = "300" className={
+                                router.pathname === "/portfolio"
+                                ? styles.strike
+                                : null
+                            }>
                             <Link href = '/portfolio'>
                                 Portfolio
                             </Link>
                         </li>
-                        <li data-aos = "fade-down" data-aos-delay = "400">
+                        <li data-aos = "fade-down" data-aos-delay = "400" className={
+                                router.pathname === "/contact"
+                                ? styles.strike
+                                : null
+                            }>
                             <Link href = '/contact'>
                                 Contact
                             </Link>
